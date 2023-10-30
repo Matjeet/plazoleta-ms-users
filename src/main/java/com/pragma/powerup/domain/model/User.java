e66ca38a3c2a0e16395fafa3f4852a7695c898bb
Mateo Velásquez Morales <matius29002@gmail.com>
2023-10-30 16:41:36 -0500

    feat: add new user class within domain/model and update database connection

---
+++ src/main/java/com/pragma/powerup/domain/model/User.java
@@ -0,0 +1,92 @@
+package com.pragma.powerup.domain.model;
+
+import java.util.Date;
+
+public class User {
+    private String name;
+    private String lastName;
+    private int documentId;
+    private int phoneNumber;
+    private Date birthDate;
+    private String email;
+    private String password;
+    private int rolId;
+
+    public User(String name, String lastName, int documentId, int phoneNumber, Date birthDate, String email, String password, int rolId) {
+        this.name = name;
+        this.lastName = lastName;
+        this.documentId = documentId;
+        this.phoneNumber = phoneNumber;
+        this.birthDate = birthDate;
+        this.email = email;
+        this.password = password;
+        this.rolId = rolId;
+    }
+
+    public User() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getDocumentId() {
+        return documentId;
+    }
+
+    public void setDocumentId(int documentId) {
+        this.documentId = documentId;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getRolId() {
+        return rolId;
+    }
+
+    public void setRolId(int rolId) {
+        this.rolId = rolId;
+    }
+}
