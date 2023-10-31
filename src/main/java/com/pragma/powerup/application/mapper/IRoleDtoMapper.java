c955445fc5b6538801a9d01533acae22a2fa6baa
Mateo Velásquez Morales <matius29002@gmail.com>
2023-10-30 20:08:25 -0500

    feat: added mapper annotation into the interface IRoleDtoMapper

--- src/main/java/com/pragma/powerup/application/mapper/IRoleDtoMapper.java
+++ src/main/java/com/pragma/powerup/application/mapper/IRoleDtoMapper.java
@@ -2,7 +2,13 @@ package com.pragma.powerup.application.mapper;
 
 import com.pragma.powerup.application.dto.RoleDTO;
 import com.pragma.powerup.domain.model.Role;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
 
+@Mapper(componentModel = "spring",
+        unmappedTargetPolicy = ReportingPolicy.IGNORE,
+        unmappedSourcePolicy = ReportingPolicy.IGNORE)
 public interface IRoleDtoMapper {
 
     RoleDTO toDto(Role role);
