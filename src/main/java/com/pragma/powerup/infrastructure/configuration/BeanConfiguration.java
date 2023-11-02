d0ed9aba4bf0b542114c65ba65072652393676ed
Mateo Velásquez Morales <matius29002@gmail.com>
2023-11-02 18:18:05 -0500

    feat: added new bean methods for user and role ports within infrastructure/configuration/BeanConfiguration.java

--- src/main/java/com/pragma/powerup/infrastructure/configuration/BeanConfiguration.java
+++ src/main/java/com/pragma/powerup/infrastructure/configuration/BeanConfiguration.java
@@ -1,11 +1,23 @@
 package com.pragma.powerup.infrastructure.configuration;
 
 import com.pragma.powerup.domain.api.IObjectServicePort;
+import com.pragma.powerup.domain.api.IRoleServicePort;
+import com.pragma.powerup.domain.api.IUserServicePort;
 import com.pragma.powerup.domain.spi.IObjectPersistencePort;
+import com.pragma.powerup.domain.spi.IRolePersistencePort;
+import com.pragma.powerup.domain.spi.IUserPersistencePort;
 import com.pragma.powerup.domain.usecase.ObjectUseCase;
+import com.pragma.powerup.domain.usecase.RoleUseCase;
+import com.pragma.powerup.domain.usecase.UserUseCase;
 import com.pragma.powerup.infrastructure.out.jpa.adapter.ObjectJpaAdapter;
+import com.pragma.powerup.infrastructure.out.jpa.adapter.RoleJpaAdapter;
+import com.pragma.powerup.infrastructure.out.jpa.adapter.UserJpaAdapter;
 import com.pragma.powerup.infrastructure.out.jpa.mapper.IObjectEntityMapper;
+import com.pragma.powerup.infrastructure.out.jpa.mapper.IRoleEntityMapper;
+import com.pragma.powerup.infrastructure.out.jpa.mapper.IUserEntityMapper;
 import com.pragma.powerup.infrastructure.out.jpa.repository.IObjectRepository;
+import com.pragma.powerup.infrastructure.out.jpa.repository.IRoleRepository;
+import com.pragma.powerup.infrastructure.out.jpa.repository.IUserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,7 +27,30 @@ import org.springframework.context.annotation.Configuration;
 public class BeanConfiguration {
     private final IObjectRepository objectRepository;
     private final IObjectEntityMapper objectEntityMapper;
+    private final IUserRepository userRepository;
+    private final IRoleRepository roleRepository;
+    private final IUserEntityMapper userEntityMapper;
+    private final IRoleEntityMapper roleEntityMapper;
 
+
+    @Bean
+    public IUserPersistencePort userPersistencePort() {
+        return new UserJpaAdapter(userRepository, userEntityMapper);
+    }
+    @Bean
+    public IUserServicePort userServicePort() {
+        return new UserUseCase(userPersistencePort());
+    }
+
+    @Bean
+    public IRolePersistencePort rolePersistencePort(){
+        return new RoleJpaAdapter(roleEntityMapper, roleRepository);
+    }
+
+    @Bean
+    public IRoleServicePort roleServicePort(){
+        return new RoleUseCase(rolePersistencePort());
+    }
     @Bean
     public IObjectPersistencePort objectPersistencePort() {
         return new ObjectJpaAdapter(objectRepository, objectEntityMapper);
