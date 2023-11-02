1fac1044d2e11e367633fe6f19f0adc15086c509
Mateo Velásquez Morales <matius29002@gmail.com>
2023-11-02 16:35:31 -0500

    feat: added new UserJpaAdapter class within infrastructure/out/jps/adapter

---
+++ src/main/java/com/pragma/powerup/infrastructure/out/jpa/adapter/UserJpaAdapter.java
@@ -0,0 +1,22 @@
+package com.pragma.powerup.infrastructure.out.jpa.adapter;
+
+import com.pragma.powerup.domain.model.User;
+import com.pragma.powerup.domain.spi.IUserPersistencePort;
+import com.pragma.powerup.infrastructure.out.jpa.entity.UserEntity;
+import com.pragma.powerup.infrastructure.out.jpa.mapper.IUserEntityMapper;
+import com.pragma.powerup.infrastructure.out.jpa.repository.IUserRepository;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class UserJpaAdapter implements IUserPersistencePort {
+
+    private final IUserRepository userRepository;
+    private final IUserEntityMapper userEntityMapper;
+    @Override
+    public void saveUser(User user) {
+        if(userRepository.findById(userEntityMapper.toEntity(user).getId()).isPresent()) {
+            throw new UserAlreadyExistsException();
+        }
+        userRepository.save(userEntityMapper.toEntity(user));
+    }
+}
