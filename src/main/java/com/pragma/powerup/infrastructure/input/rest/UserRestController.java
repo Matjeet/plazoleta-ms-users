e91d39ee5083b654c27bf8d1f9f03c9e601ab778
Mateo Velásquez Morales <matius29002@gmail.com>
2023-11-01 19:50:17 -0500

    feat: added new UserRestController class within infrastructure/input/rest

---
+++ src/main/java/com/pragma/powerup/infrastructure/input/rest/UserRestController.java
@@ -0,0 +1,25 @@
+package com.pragma.powerup.infrastructure.input.rest;
+
+import com.pragma.powerup.application.dto.request.UserRequestDTO;
+import com.pragma.powerup.application.handler.IUserHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class UserRestController {
+
+    private final IUserHandler userHandler;
+
+    @PostMapping("/")
+    public ResponseEntity<Void> saveUser(@RequestBody UserRequestDTO userRequestDTO){
+        userHandler.saveUser(userRequestDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+}
