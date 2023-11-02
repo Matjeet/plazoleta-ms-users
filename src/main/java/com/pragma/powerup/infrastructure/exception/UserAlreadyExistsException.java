0d78b8b1da6caaefdc3f60106553efe893a75ea5
Mateo Velásquez Morales <matius29002@gmail.com>
2023-11-02 16:39:18 -0500

    feat: added new UserAlreadyExistsException class within infrastructure/exception

---
+++ src/main/java/com/pragma/powerup/infrastructure/exception/UserAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.pragma.powerup.infrastructure.exception;
+
+public class UserAlreadyExistsException extends RuntimeException{
+    public UserAlreadyExistsException(){
+        super();
+    }
+}
