751018e1158708aa949edb55fc33984554e9de71
Mateo Velásquez Morales <matius29002@gmail.com>
2023-11-04 18:53:22 -0500

    style: improvements suggested by SonarLint into application packages

--- src/main/java/com/pragma/powerup/application/mapper/IRoleDtoMapper.java
+++ src/main/java/com/pragma/powerup/application/mapper/IRoleDtoMapper.java
@@ -3,7 +3,6 @@ package com.pragma.powerup.application.mapper;
 import com.pragma.powerup.application.dto.RoleDTO;
 import com.pragma.powerup.domain.model.Role;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring",
