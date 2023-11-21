42509dedae559e60525f16e13f27048970b7ce41
Mateo Velásquez Morales <matius29002@gmail.com>
2023-11-21 18:21:02 -0500

    chore: deleted unnecessary imports

--- src/main/java/com/pragma/powerup/application/dto/response/AuthResponseDto.java
+++ src/main/java/com/pragma/powerup/application/dto/response/AuthResponseDto.java
@@ -1,10 +1,7 @@
 package com.pragma.powerup.application.dto.response;
 
-import com.pragma.powerup.application.dto.RoleDTO;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.time.LocalDate;
 
 @Getter
 @Setter
