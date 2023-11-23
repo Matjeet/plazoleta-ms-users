8b66ba1f0b56185fd18b094545d3f2a60cb0883f
Mateo Velásquez Morales <matius29002@gmail.com>
2023-11-23 17:53:57 -0500

    feat: changed IUserResponseMapper interface to IAuthResponseMapper interface

--- src/main/java/com/pragma/powerup/application/handler/impl/UserHandler.java
+++ src/main/java/com/pragma/powerup/application/handler/impl/UserHandler.java
@@ -1,11 +1,13 @@
 package com.pragma.powerup.application.handler.impl;
 
 import com.pragma.powerup.application.dto.request.RegisterRequestDto;
+import com.pragma.powerup.application.dto.response.AuthResponseDto;
 import com.pragma.powerup.application.handler.IPasswordHandler;
+import com.pragma.powerup.application.handler.ITokenHandler;
 import com.pragma.powerup.application.handler.IUserHandler;
 import com.pragma.powerup.application.mapper.IRoleDtoMapper;
 import com.pragma.powerup.application.mapper.request.IUserRequestMapper;
-import com.pragma.powerup.application.mapper.response.IUserResponseMapper;
+import com.pragma.powerup.application.mapper.response.IAuthResponseMapper;
 import com.pragma.powerup.domain.api.IRoleServicePort;
 import com.pragma.powerup.domain.api.IUserServicePort;
 import com.pragma.powerup.domain.model.Role;
