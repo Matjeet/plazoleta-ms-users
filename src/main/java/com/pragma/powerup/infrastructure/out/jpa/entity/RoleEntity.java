0f35728ccfe8b843006ceef1931c84821a597018
Mateo Velásquez Morales <matius29002@gmail.com>
2023-11-22 21:55:43 -0500

    feat: now the RoleEntity class implements the GrantedAuthority interface

--- src/main/java/com/pragma/powerup/infrastructure/out/jpa/entity/RoleEntity.java
+++ src/main/java/com/pragma/powerup/infrastructure/out/jpa/entity/RoleEntity.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
 
 import javax.persistence.*;
 
@@ -14,10 +15,15 @@ import javax.persistence.*;
 @NoArgsConstructor
 @Getter
 @Setter
-public class RoleEntity {
+public class RoleEntity implements GrantedAuthority {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
+
+    @Override
+    public String getAuthority() {
+        return name;
+    }
 }
