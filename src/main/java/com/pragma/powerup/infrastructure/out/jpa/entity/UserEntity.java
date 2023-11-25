9adc7ae699f2a2695024755e2c29286bbc288f8e
Mateo Velásquez Morales <matius29002@gmail.com>
2023-11-25 16:32:12 -0500

    chore: remove UserDetails implementation in UserEntity

--- src/main/java/com/pragma/powerup/infrastructure/out/jpa/entity/UserEntity.java
+++ src/main/java/com/pragma/powerup/infrastructure/out/jpa/entity/UserEntity.java
@@ -1,14 +1,8 @@
 package com.pragma.powerup.infrastructure.out.jpa.entity;
 
 import lombok.*;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
 import javax.persistence.*;
 import java.time.LocalDate;
-import java.util.Collection;
-import java.util.List;
 
 @Entity
 @Table(name = "user")
@@ -16,7 +10,7 @@ import java.util.List;
 @NoArgsConstructor
 @Getter
 @Setter
-public class UserEntity implements UserDetails {
+public class UserEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -30,35 +24,5 @@ public class UserEntity implements UserDetails {
     private String password;
     @ManyToOne
     @JoinColumn(name = "role_id", nullable = false)
-    private transient RoleEntity role;
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority(role.getName()));
-    }
-
-    @Override
-    public String getUsername() {
-        return email;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
+    private RoleEntity role;
 }
