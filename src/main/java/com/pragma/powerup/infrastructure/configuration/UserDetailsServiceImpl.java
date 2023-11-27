fab08001adeab26033d3f1133b3aad36d8af85bf
Mateo Velásquez Morales <matius29002@gmail.com>
2023-11-26 22:10:52 -0500

    feat: added new UserDetailsServiceImpl class

---
+++ src/main/java/com/pragma/powerup/infrastructure/configuration/UserDetailsServiceImpl.java
@@ -0,0 +1,19 @@
+package com.pragma.powerup.infrastructure.configuration;
+
+import com.pragma.powerup.infrastructure.out.jpa.repository.IUserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+@Configuration
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl {
+
+    private final IUserRepository userRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new CustomUserDetailsService(userRepository);
+    }
+}
