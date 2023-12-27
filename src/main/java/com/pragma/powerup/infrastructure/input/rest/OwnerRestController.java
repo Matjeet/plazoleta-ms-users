69f4b5bcb91448b909dd27980c8dfc6f1b47a3af
Mateo Velásquez Morales <matius29002@gmail.com>
2023-12-27 12:20:23 -0500

    feat: added annotation that protecting the validateOwnerRole method and added annotations about the documentation

--- src/main/java/com/pragma/powerup/infrastructure/input/rest/OwnerRestController.java
+++ src/main/java/com/pragma/powerup/infrastructure/input/rest/OwnerRestController.java
@@ -1,9 +1,14 @@
 package com.pragma.powerup.infrastructure.input.rest;
 
 import com.pragma.powerup.application.handler.IUserHandler;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +21,14 @@ public class OwnerRestController {
 
     private final IUserHandler userHandler;
 
+    @Operation(summary = "Validate whether or no a user is an owner")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "It's an owner", content = @Content),
+            @ApiResponse(responseCode = "401", description = "It's not an owner", content = @Content),
+            @ApiResponse(responseCode = "403", description = "You don't have permissions", content = @Content)
+    })
     @GetMapping("/{id}")
+    @PreAuthorize("hasRole('ROLE_administrador')")
     public ResponseEntity<Boolean> validateOwnerRole(@PathVariable int id){
         boolean ownerRole = userHandler.validateOwnerRole(id);
         if(ownerRole){
