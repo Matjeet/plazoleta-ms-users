ab85efff11ce28a1d38a6d2c8016292175315509
Mateo Velásquez Morales <matius29002@gmail.com>
2024-01-16 13:21:12 -0500

    feat: new RestaurantEmployeeEntity within infrastructure

--- src/main/java/com/pragma/powerup/domain/Constants.java
+++ src/main/java/com/pragma/powerup/domain/Constants.java
@@ -9,6 +9,6 @@ public class Constants {
     public static final String TOKEN_ROLE_OWNER = "ROLE_propietario";
     public static final String USER = "user";
     public static final String ROLE = "role";
-
     public static final String OWNER = "propietario";
+    public static final String RESTAURANT_EMPLOYEE = "restaurant_employee";
 }
--- src/main/java/com/pragma/powerup/infrastructure/out/jpa/entity/RestaurantEmployee.java
+++ src/main/java/com/pragma/powerup/infrastructure/out/jpa/entity/RestaurantEmployee.java
@@ -1,2 +1,27 @@
-package com.pragma.powerup.infrastructure.out.jpa.entity;public class RestaurantEmployee {
+package com.pragma.powerup.infrastructure.out.jpa.entity;
+
+import com.pragma.powerup.domain.Constants;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = Constants.RESTAURANT_EMPLOYEE)
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class RestaurantEmployee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private int idRestaurant;
+
+    @ManyToOne
+    @JoinColumn(name = "employee_id", nullable = false)
+    private UserEntity employee;
 }
