a59b7d749efad85044225ddb15fd4f68a47eeb74
Mateo Velásquez Morales <matius29002@gmail.com>
2024-01-20 17:44:05 -0500

    feat: new ClientRestController within infrastructure

--- src/main/java/com/pragma/powerup/infrastructure/input/rest/ClientRestController.java
+++ src/main/java/com/pragma/powerup/infrastructure/input/rest/ClientRestController.java
@@ -1,16 +1,29 @@
 package com.pragma.powerup.infrastructure.input.rest;
 
 import com.pragma.powerup.application.dto.response.UserInfoResponseDto;
+import com.pragma.powerup.application.handler.IUserHandler;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/user/v1")
+@RequestMapping("/client/v1")
 @RequiredArgsConstructor
-public class UserRestController {
+public class ClientRestController {
 
-    public ResponseEntity<UserInfoResponseDto> getUser(@PathVariable idUser)
+    private final IUserHandler userHandler;
+
+    @PreAuthorize("hasRole('ROLE_empleado')")
+    @GetMapping("/info/{idClient}")
+    public ResponseEntity<UserInfoResponseDto> getClient(@PathVariable int idClient){
+        UserInfoResponseDto result = userHandler.getClient(idClient);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(result);
+    }
 }
