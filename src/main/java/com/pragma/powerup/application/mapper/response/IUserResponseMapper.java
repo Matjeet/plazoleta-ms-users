a38feae3049a619895f3b065301a0ad6684ade36
Mateo Velásquez Morales <matius29002@gmail.com>
2024-01-20 16:00:23 -0500

    feat: new IUserResponseMapper interface within application

--- src/main/java/com/pragma/powerup/application/mapper/response/IUserResponseMapper.java
+++ src/main/java/com/pragma/powerup/application/mapper/response/IUserResponseMapper.java
@@ -1,2 +1,14 @@
-package com.pragma.powerup.application.mapper.response;public interface IUserResponseMapper {
+package com.pragma.powerup.application.mapper.response;
+
+import com.pragma.powerup.application.dto.response.UserInfoResponseDto;
+import com.pragma.powerup.domain.model.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring",
+        unmappedSourcePolicy = ReportingPolicy.IGNORE,
+        unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface IUserResponseMapper {
+
+    UserInfoResponseDto toUserInfoDto(User user);
 }
